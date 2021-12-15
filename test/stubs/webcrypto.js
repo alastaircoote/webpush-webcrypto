73572ae23af4c1f945431e2a63425ab0850e15e7
Alastair Coote <alastair.coote@nytimes.com>
2021-12-15 09:09:56 -0500

    delete all

--- test/stubs/webcrypto.js
+++
@@ -1,6 +0,0 @@
-import cryptoModule from "crypto";
-// doing some stupid typecasting stuff here to get around incorrect TS types
-const wc = /** @type {unknown} */ (cryptoModule.webcrypto);
-const crypto = /** @type {Crypto} */ (wc);
-
-export default crypto;
